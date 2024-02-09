a93d15fe36e21c4b552469daaff02108ef6fcf84
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2024-02-09 19:43:08 +0100

    Pairwise changes to dynamic parameters

--- run.sh
+++ run.sh
@@ -9,3 +9,20 @@ gcc -o3 hill.c -lm -o hill.ce
 ./time-series.ce > tmp-t &
 ./stoch.ce > tmp-s &
 ./hill.ce > tmp-h &
+
+gcc -o3 stoch-scan.c -lm -o stoch-scan.ce
+./stoch-scan.ce 0 > tmp-ss-0 &
+./stoch-scan.ce 1 > tmp-ss-1 &
+./stoch-scan.ce 2 > tmp-ss-2 &
+./stoch-scan.ce 3 > tmp-ss-3 &
+./stoch-scan.ce 4 > tmp-ss-4 &
+./stoch-scan.ce 5 > tmp-ss-5 &
+./stoch-scan.ce 6 > tmp-ss-6 &
+./stoch-scan.ce 7 > tmp-ss-7 &
+./stoch-scan.ce 8 > tmp-ss-8 &
+
+gcc -o3 param-matrix.c -lm -o param-matrix.ce
+./param-matrix.ce 2 0 > tmp-pm-0 &
+./param-matrix.ce 2 1 > tmp-pm-1 &
+./param-matrix.ce 2 2 > tmp-pm-2 &
+./param-matrix.ce 2 3 > tmp-pm-3 &
